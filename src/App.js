1dc76325c1986994cb4fc38c9362411f12042a33
areisle <areisle@gmail.com>
2019-02-12 22:54:40 -0800

    translate to hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Person from './Person';
 import "./App.css";
 
 let renders = 0;
 
-class App extends React.Component {
+function App(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            people: [],
-            page: 1,
-            pageSize: 10,
-            count: 0,
-        }
+    const pageSize = 10;
+    const [people, setPeople ] = useState([]);
+    const [next, setNext ] = useState(null);
+    const [previous, setPrevious ] = useState(null);
+    const [page, setPage ] = useState(1);
+    const [count, setCount ] = useState(0);
+    const [request, setRequest] = useState(null);
 
-    }
+    useEffect(() => {
+        getPage('https://swapi.co/api/people');
+        return () => request.abort();
+    }, [])
 
-    async componentDidMount() {
-        this.getPage('https://swapi.co/api/people');
-    }
-
-    getPage = async (url) => {
+    const getPage = async (url) => {
 
         if (!url) { return; }
 
-        if (this.request) {
-            this.request.abort();
+        if (request) {
+            request.abort();
             console.log('aborting request')
         }
 
-        this.request = new AbortController();
+        const newRequest = new AbortController();
+        setRequest(newRequest);
 
         try {
             const result = await fetch(url, {
-                signal: this.request.signal,
+                signal: newRequest.signal,
             });
 
             console.log(result)
@@ -45,64 +44,48 @@ class App extends React.Component {
 
             const { results: people, next, count, previous } = await result.json();
 
-            this.setState({
-                people,
-                count,
-                next,
-                previous,
-            });
+            setPeople(people);
+            setNext(next);
+            setCount(count);
+            setPrevious(previous);
 
         } catch (e) {
             console.error(e);
-        }  
+        }
     }
 
-    nextPage = async () => {
-        const { next, page } = this.state;
-        await this.getPage(next);
-        this.setState({
-            page: page + 1,
-        })
+    const nextPage = async () => {
+        await getPage(next);
+        setPage(page + 1)
     }
 
-    previousPage = async () => {
-        const { previous, page } = this.state;
-        await this.getPage(previous);
-        this.setState({
-            page: page - 1,
-        })
+    const previousPage = async () => {
+        await getPage(previous);
+        setPage(page - 1);
     }
-
-    componentWillUnmount() {
-        this.request.abort();
-    }
-
-    render() {
-        console.log(++renders)
-        const { people, page, pageSize, count, next, previous } = this.state;
-        return (
-            <div className="App">
-                {people.map(person => (<Person key={person.name} {...person}/>))}
-                <div className='pagination'>
-                    <button 
-                        onClick={this.previousPage}
-                        disabled={!previous}
-                    >
-                        previous
-                    </button>
-                    <div className='pagination__count'>
-                        showing {(page - 1) * pageSize  + 1} - {Math.min((page) * pageSize,  count || 0) } of {count}
-                    </div>
-                    <button 
-                        onClick={this.nextPage}
-                        disabled={!next}
-                    >
-                        next
-                    </button>
+    console.log(++renders)
+    return (
+        <div className="App">
+            {people.map(person => (<Person key={person.name} {...person}/>))}
+            <div className='pagination'>
+                <button 
+                    onClick={previousPage}
+                    disabled={!previous}
+                >
+                    previous
+                </button>
+                <div className='pagination__count'>
+                    showing {(page - 1) * pageSize  + 1} - {Math.min((page) * pageSize,  count || 0) } of {count}
                 </div>
+                <button 
+                    onClick={nextPage}
+                    disabled={!next}
+                >
+                    next
+                </button>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default App;
